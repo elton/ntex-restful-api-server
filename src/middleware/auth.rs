2d8c3e1182807adf271466a24cbd00f191f89c10
Elton Zheng <elton.zheng.sh@gmail.com>
2024-03-19 22:37:27 +0800

    add preflight check in auth middleware

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,8 +1,10 @@
+use ntex::http::{header, Method};
 use ntex::service::{Middleware, Service, ServiceCtx};
 use ntex::web::{Error, ErrorRenderer, WebRequest, WebResponse};
 use ntex::{http, web};
 
 use crate::errors::AppError;
+use crate::handlers::Response;
 use crate::repository;
 use crate::utils::jwt;
 
@@ -37,54 +39,81 @@ where
         req: WebRequest<Err>,
         ctx: ServiceCtx<'_, Self>,
     ) -> Result<Self::Response, Self::Error> {
-        log::info!("path: {}", req.path());
-        // Get the token from the request headers
-        if let Some(token) = req.headers().get(http::header::AUTHORIZATION) {
-            let token = token.to_str().unwrap().replace("Bearer ", "");
-            log::info!("token: {:?}", token);
+        // check preflight request
+        if Method::OPTIONS == req.head().method {
+            let mut res = ctx.call(&self.service, req).await?;
+            res.headers_mut().insert(
+                http::header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                http::header::HeaderValue::from_static("*"),
+            );
+            Ok(res)
+        } else {
+            // Get the token from the request headers
+            if let Some(token) = req.headers().get(http::header::AUTHORIZATION) {
+                let token = token.to_str().unwrap().replace("Bearer ", "");
+                log::info!("token: {:?}", token);
 
-            // Verify the token
-            // Get a connection to the Redis server
-            let mut conn = repository::redis::new()
-                .ok()
-                .unwrap()
-                .clone()
-                .get_multiplexed_async_connection()
-                .await
-                .ok()
-                .unwrap();
-
-            // Get the user_id from the token
-            if let Some(user_id) =
-                jwt::get_user_id_from_redis(&mut conn, jwt::TokenType::AccessToken, &token)
-                    .await
-                    .map_err(|e| {
-                        log::error!("Error getting user_id from redis: {}", e);
-                        AppError::InternalServerError(e.to_string())
-                    })
+                // Verify the token
+                // Get a connection to the Redis server
+                let mut conn = repository::redis::new()
                     .ok()
-            {
-                log::info!("user_id: {:?}", user_id);
-                //if get user_id, Call the next service in the chain
-                let res = ctx.call(&self.service, req).await?;
-                Ok(res)
+                    .unwrap()
+                    .clone()
+                    .get_multiplexed_async_connection()
+                    .await
+                    .ok()
+                    .unwrap();
+
+                // Get the user_id from the token
+                if let Some(user_id) =
+                    jwt::get_user_id_from_redis(&mut conn, jwt::TokenType::AccessToken, &token)
+                        .await
+                        .map_err(|e| {
+                            log::error!("Error getting user_id from redis: {}", e);
+                            AppError::InternalServerError(e.to_string())
+                        })
+                        .ok()
+                {
+                    log::info!("user_id: {:?}", user_id);
+                    //if get user_id, Call the next service in the chain
+                    let mut res = ctx.call(&self.service, req).await?;
+                    res.headers_mut().insert(
+                        http::header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                        http::header::HeaderValue::from_static("*"),
+                    );
+                    Ok(res)
+                } else {
+                    log::info!("Invalid token");
+                    // If no token is found, redirect to the login page
+                    if req.path() == "/api/v1/users/login" {
+                        ctx.call(&self.service, req).await
+                    } else {
+                        Ok(req.into_response("Invalid token".to_string()))
+                    }
+                }
+            // If no token is found, redirect to the login page
             } else {
-                log::info!("Invalid token");
+                log::error!("No token found");
                 // If no token is found, redirect to the login page
                 if req.path() == "/api/v1/users/login" {
-                    ctx.call(&self.service, req).await
+                    let mut res = ctx.call(&self.service, req).await;
+                    res.as_mut().unwrap().headers_mut().insert(
+                        http::header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                        http::header::HeaderValue::from_static("*"),
+                    );
+                    res
                 } else {
-                    Ok(req.into_response(redirect_to_login()))
+                    Ok(
+                        req.into_response(web::HttpResponse::Unauthorized().json(
+                            &Response::<()> {
+                                status: "fail".to_string(),
+                                message: "No token found".to_string(),
+                                count: None,
+                                data: None,
+                            },
+                        )),
+                    )
                 }
-            }
-        // If no token is found, redirect to the login page
-        } else {
-            log::error!("No token found");
-            // If no token is found, redirect to the login page
-            if req.path() == "/api/v1/users/login" {
-                ctx.call(&self.service, req).await
-            } else {
-                Ok(req.into_response(redirect_to_login()))
             }
         }
     }
